2cc28b2deddedd490a02a26d26b29f85c655cb4c
Addy Yang <yang.addy@hotmail.com>
2022-10-12 16:39:51 -0400

    Update dependencies

--- backend/src/main/java/com/example/location/LocationApplication.java
+++ backend/src/main/java/com/example/location/LocationApplication.java
@@ -3,10 +3,6 @@ package com.example.location;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class LocationApplication {
 
