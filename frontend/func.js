233720a6e95c2767256149e04ef9b5c2f809acf4
zhangxisheng <xzhang3033@gatech.edu>
2022-10-12 21:06:49 -0400

    location search

--- frontend/func.js
+++ frontend/func.js
@@ -1,18 +1,20 @@
 var map;
+var service;
+var infowindow;
 
 function myMap() {
   var mapProp = {
-      center: new google.maps.LatLng(33.775373, -84.396847),
-      zoom: 12,
+    center: new google.maps.LatLng(33.775373, -84.396847),
+    zoom: 12,
   };
   map = new google.maps.Map(document.getElementById('map'), mapProp);
 }
 
 
 function setup() {
-  var inputAddress1 = document.getElementById('latBox').value;
-  var inputAddress2 = document.getElementById('lonBox').value;
-  var center = new google.maps.LatLng(inputAddress1,inputAddress2);
+  var inputAddress1 = Number(document.getElementById('latBox').value);
+  var inputAddress2 = Number(document.getElementById('lonBox').value);
+  var center = new google.maps.LatLng(inputAddress1, inputAddress2);
   var radius = document.getElementById('radBox').value;
   //test if the inputs get read
   //alert("your radius input: " + radius + ", and your coordinate input: " + center);
@@ -24,5 +26,35 @@ function setup() {
   var marker = new google.maps.Marker({
     map: map,
     position: center
+  });
+  var request = {
+    location: center,
+    radius: radius,
+    type: ['restaurant']
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+  console.log(map);
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
   });
 }
